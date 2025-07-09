462513ded2d34405c25002f15b9c0b934fc6cb94
unknown <alexadoniev@gmail.com>
2025-07-09 18:39:40 +0300

    add head scripts

--- app/layout.js
+++ app/layout.js
@@ -50,7 +50,40 @@ export default function RootLayout({ children }) {
           href="/assets/icons/favicon.svg"
           type="image/x-icon"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17078124366"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        
+        window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17078124366');
+
+        `,
+          }}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "sc9r7gn6pd");
+
+          
+          `,
+          }}
+        ></script>
       </head>
+
       <body className={`${heebo.variable} ${ptSerif.variable}`}>
         <Providers>{children}</Providers>
       </body>
