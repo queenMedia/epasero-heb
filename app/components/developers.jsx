07d3423c83bbc6a4eec3bef340359badb7cfa7e2
unknown <alexadoniev@gmail.com>
2025-07-09 11:45:04 +0300

    new partners

--- app/components/developers.jsx
+++ app/components/developers.jsx
@@ -6,33 +6,67 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
 const developersData = [
-  { image: "assets/partners/1.png" },
-  { image: "assets/partners/2.png" },
-  { image: "assets/partners/3.png" },
-  { image: "assets/partners/4.png" },
-  { image: "assets/partners/5.png" },
-  { image: "assets/partners/6.png" },
-  { image: "assets/partners/7.png" },
-  { image: "assets/partners/8.png" },
-  { image: "assets/partners/9.png" },
-  { image: "assets/partners/10.png" },
-  { image: "assets/partners/11.png" },
-  { image: "assets/partners/12.png" },
-  { image: "assets/partners/13.png" },
+  { image: "assets/partners/new-imgs/358.png" },
+  { image: "assets/partners/new-imgs/union-properties-logo.png.webp" },
+  { image: "assets/partners/new-imgs/118.png" },
+  { image: "assets/partners/new-imgs/tiger-group-logo.png.webp" },
+  { image: "assets/partners/new-imgs/sobha-realty-logo.png.webp" },
+  { image: "assets/partners/new-imgs/select-group-logo.svg" },
+  { image: "assets/partners/new-imgs/Samana.png" },
+  { image: "assets/partners/new-imgs/Riviera-e1740061068284.png" },
+  {
+    image: "assets/partners/new-imgs/revolution.png",
+  },
+  { image: "assets/partners/new-imgs/logo_rak.png" },
+  { image: "assets/partners/new-imgs/logo.png" },
+  { image: "assets/partners/new-imgs/peacehome.webp" },
+  { image: "assets/partners/new-imgs/pantheon-dev.png" },
+  { image: "assets/partners/new-imgs/ORO24.png" },
+  { image: "assets/partners/new-imgs/Omniyat-e1740061081561.png" },
+  { image: "assets/partners/new-imgs/object-1-logo-black.png.webp" },
+  { image: "assets/partners/new-imgs/nakheel-logo.svg" },
+  { image: "assets/partners/new-imgs/Meraas1.svg" },
+  { image: "assets/partners/new-imgs/252.webp" },
+  { image: "assets/partners/new-imgs/mag-logo.svg" },
+  { image: "assets/partners/new-imgs/cropped-LEOS-Logo.webp" },
+  { image: "assets/partners/new-imgs/Imtiaz-logo.png.webp" },
+  { image: "assets/partners/new-imgs/iman-developers.webp" },
+  { image: "assets/partners/new-imgs/invest-group-overseas-logo.png" },
+  { image: "assets/partners/new-imgs/Fakhruddin.webp" },
+  { image: "assets/partners/new-imgs/Emaar_logo.svg" },
+  { image: "assets/partners/new-imgs/ellington.svg" },
+  { image: "assets/partners/new-imgs/8b651d5d4efeb6bb3859e307693edad5.jpg" },
+  { image: "assets/partners/new-imgs/deyaar-logo.svg" },
+  { image: "assets/partners/new-imgs/dar-logo.png" },
+  { image: "assets/partners/new-imgs/Danube-Properties.svg" },
+  { image: "assets/partners/new-imgs/damac-logo.svg" },
+  { image: "assets/partners/new-imgs/Дизайн-без-названия.png" },
+  { image: "assets/partners/new-imgs/images.png" },
+  { image: "assets/partners/new-imgs/Binghatti-Logo.svg" },
+  { image: "assets/partners/new-imgs/dev_img_185_beyond-logo.png" },
+  { image: "assets/partners/new-imgs/1735912312552.jpeg" },
+  { image: "assets/partners/new-imgs/arada-developer-logo.svg" },
+  { image: "assets/partners/new-imgs/aqua-dubai.png" },
+  {
+    image:
+      "assets/partners/new-imgs/a002a5_ebe0662c4bee43f38c267c6e60af2114mv2.png",
+  },
+  { image: "assets/partners/new-imgs/AlFuttaimlogoEn.png" },
+  { image: "assets/partners/new-imgs/aldar-logo.svg" },
 ];
 
 const responsive = {
   superLargeDesktop: {
     breakpoint: { max: 4000, min: 1024 },
-    items: 7,
+    items: 6,
   },
   desktop: {
     breakpoint: { max: 1024, min: 768 },
-    items: 4,
+    items: 5,
   },
   tablet: {
     breakpoint: { max: 768, min: 464 },
-    items: 3,
+    items: 4,
   },
   tablet: {
     breakpoint: { max: 568, min: 0 },
@@ -61,8 +95,8 @@ const Developers = ({ developers = developersData }) => {
         {developers.map((developer, index) => (
           <div key={index} className="carousel-item">
             <Image
-              width={150}
-              height={100}
+              width={213}
+              height={76}
               src={developer.image}
               alt="developer"
             />
